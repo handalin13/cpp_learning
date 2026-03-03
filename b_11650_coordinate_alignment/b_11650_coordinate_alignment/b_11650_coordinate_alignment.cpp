996749aa790c7959262abd2212cc0e419ca6b264
handalin13 <handalin@yonsei.ac.kr>
2026-03-04 04:45:39 +0900

    Add solution for baekjoon 11650 (좌표 정렬하기)

---
+++ b_11650_coordinate_alignment/b_11650_coordinate_alignment/b_11650_coordinate_alignment.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int num_dot;
+    int x, y;
+    vector<pair<int, int>> xy;
+    std::cin >> num_dot;
+
+    for (int i = 0; i < num_dot; i++) {
+        std::cin >> x >> y;
+        xy.push_back({ x,y });
+
+    }
+
+    sort(xy.begin(), xy.end()); // O(N log N)
+    for (int i = 0; i < num_dot; i++) {
+        std::cout << xy[i].first << " " << xy[i].second << '\n';
+    }
+    return 0;
+}
+
+/*
+bool comp(const std::pair<int,int>& a, const std::pair<int,int>& b) {
+    if (a.first != b.first) return a.first < b.first;
+    return a.second < b.second;
+}
+*/
